3e5193370d90693f0d19133013c79a433cc505b5
leeviojala <leevi.ojala@gmail.com>
2017-11-15 13:59:45 +0200

    Database connection

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,20 +5,23 @@ import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import IconButton from 'material-ui/IconButton';
 import PersonOutline from 'material-ui/svg-icons/social/person-outline';
+import axios from 'axios';
 
 class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: ''
+            value: '',
+            roomsConversations: []
         };
 
         this.chatClick = this.chatClick.bind(this);
+        this.getChats();
         console.log(this);
     }
 
     chatClick(e) {
-        
+        this.props.sideClick(e.target.dataset.id);
     }
 
     myColor(position) {
@@ -28,11 +31,33 @@ class Sidebar extends Component {
         return "";
     }
 
-    toggle(id, pos) {
-        if (this.state.active !== pos) {
-            this.setState({ active: pos });
-            this.props.sideClick(id);
+    toggle(position) {
+        if (this.state.active === position) {
+            this.setState({ active: null })
+        } else {
+            this.setState({ active: position })
         }
+    }
+
+    getChats() {
+        axios.get('/conversations?user_id=297116670178000897')
+            .then((response) => {
+                console.log(response.data);
+                let data = [];
+                let rooms = response.data.rooms;
+                let conversations = response.data.conversations;
+                if (rooms !== null) {
+                    data.push.apply(data, rooms);
+                }
+
+                if (conversations !== null) {
+                    data.push.apply(data, conversations)
+                }
+                this.setState({ roomsConversations: data });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     render() {
@@ -41,38 +66,20 @@ class Sidebar extends Component {
                 <div className="sidebar-header">
                     Riku Wikman
                 </div>
-                <div className="message-preview" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(50, 0) }}>
-                    <div className="message-preview-header" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(50, 0) }}>
-                        Leevi Ojala
-                    </div>
-                    <div className="message-preview-text" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(50, 0) }}>
-                        Äijä on kyllä ihan paras, ihan paras, ihan paras!
-                    </div>
-                </div>
-                <div className="message-preview" style={{ background: this.myColor(1) }} onClick={() => { this.toggle(40, 1) }}>
-                    <div className="message-preview-header" style={{ background: this.myColor(1) }} onClick={() => { this.toggle(40, 1) }}>
-                        Santeri Remes
-                    </div>
-                    <div className="message-preview-text" style={{ background: this.myColor(1) }} onClick={() => { this.toggle(40, 1) }}>
-                        Se on GG.
-                    </div>
-                </div>
-                <div className="message-preview">
-                    <div className="message-preview-header">
-                        Kullihiiri
-                    </div>
-                    <div className="message-preview-text">
-                        Eemeli: Lähteekö kukaan leffaan tänään?
-                    </div>
-                </div>
-                <div className="message-preview">
-                    <div className="message-preview-header">
-                        Leevi Ojala
-                    </div>
-                    <div className="message-preview-text">
-                        Äijä on kyllä ihan paras, ihan paras, ihan paras!
-                    </div>
-                </div>
+                {this.state.roomsConversations.map((room, i) => {
+                    return ([
+                        <div className="message-preview" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(0) }} data-id={50} onClick={this.chatClick}>
+                            <div className="message-preview-header" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(0) }} data-id={50}>
+                                {room.Name}
+                            </div>
+                            <div className="message-preview-text" style={{ background: this.myColor(0) }} onClick={() => { this.toggle(0) }} data-id={50}>
+                                Äijä on kyllä ihan paras, ihan paras, ihan paras!
+                            </div>
+                        </div>
+                    ]);
+                })}
+
+
             </div>
         );
     }
